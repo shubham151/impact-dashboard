8f9d3e8d906d41e7ba5bb6e1c4635b4f766aa957
Shubham Mishra <shubham.cmishra@gmail.com>
2026-05-11 00:50:17 -0700

    add time out

--- api/data.ts
+++ api/data.ts
@@ -1,10 +1,21 @@
 import { Db } from '../server/Db.js'
 import { Impact } from '../server/Impact.js'
 
+const TIMEOUT_MS = 25_000
+
+function withTimeout<T>(promise: Promise<T>, ms: number, label: string): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) =>
+      setTimeout(() => reject(new Error(`${label} timed out after ${ms}ms`)), ms)
+    )
+  ])
+}
+
 export default async function handler(): Promise<Response> {
   try {
     const db = Db.get()
-    const report = await Impact.compute(db)
+    const report = await withTimeout(Impact.compute(db), TIMEOUT_MS, 'Impact.compute')
     return Response.json(report, {
       status: 200,
       headers: { 'Cache-Control': 'public, s-maxage=300, stale-while-revalidate=600' }
